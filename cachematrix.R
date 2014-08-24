1084a8fe004e9306d31ac2ffbad615ece0aed442
Dhruv Pancholi <pancholidhruv@gmail.com>
2014-08-25 01:25:09 +0530

    Completed Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To run this code first need to create a cacheMatrix object using makeCacheMatrix function
+## Now to access the inverse of the matrix, cacheSolve function with the CacheMatrix object as parameter
 
-## Write a short comment describing this function
+## This function creates a new type of object which is a cachable matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y) {
+	        x <<- y
+	        m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set, get = get,
+	     setinverse = setinverse,
+	     getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function solves the given cached matrix, if the cached data is
+## present in the elevated environment then that value is returned
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m)) {
+		message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
